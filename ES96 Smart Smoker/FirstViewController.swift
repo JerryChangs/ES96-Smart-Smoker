212998c78629e64698c673faee1dc8ef0f16046c
Jerry Chang <JerryChang@college.harvard.edu>
2015-04-28 15:14:11 -0400

    Initial Commit

---
+++ ES96 Smart Smoker/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ES96 Smart Smoker
+//
+//  Created by Jerry Chang on 4/28/15.
+//  Copyright (c) 2015 JerryChang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
